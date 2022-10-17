4fd8cf35d7d0143cd5ac22f2237f9b55f33d583c
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-17 08:57:00 +0200

    update

--- script.js
+++ script.js
@@ -7,8 +7,33 @@ function sync(){
 }
 
 function verwerkPerceel(tags){
-    console.log(tags);
-    for(let t of tags){
-        $("#uitvoer").append('<p>' + t.getAttribute("k") + '=' + t.getAttribute("v") + '</p>');
+    let p = new Perceel();
+    p.laadOSM(tags);
+    $("#uitvoer").append('<a href="' + p.OATURI() + '" target="_blank">Ga naar OAT-def</a>');
+}
+
+class Perceel {
+    constructor() {
+        this.gemeente = null;
+        this.sectie = null;
+        this.perceelnr = null;
+        this.perceelnrtvg = null;
+    }
+
+    laadOSM(tags){
+        for(let t of tags){
+            let k = t.getAttribute("k");
+            let v = t.getAttribute("v");
+            switch(k){
+                case 'kad:gemeente': this.gemeente = v; break;
+                case 'kad:sectie': this.sectie = v; break;
+                case 'kad:perceelnr': this.perceelnr = v; break;
+                case 'kad:perceelnrtvg': this.perceelnrtvg = v; break;
+            }
+        }
+    }
+
+    get OATURI(){
+        return 'https://oat.hisgis.nl/oat-ws/rest/percelen/' + this.gemeente + '/' + this.sectie + '/' + this.perceelnr + (this.perceelnrtvg ? '/' + this.perceelnrtvg : '');
     }
 }
