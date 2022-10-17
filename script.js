7890a3d99966fa69e93a91811a2a17bd20fcb5df
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-17 08:13:05 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function sync(){
     let osmid = $("#osmid").val();
-    alert(osmid);
+    fetch('https://osm.hisgis.nl/api/0.6/way/' + osmid + '/')
+  .then((response) => response.xml())
+  .then((data) => console.log(data));
 }
