17784164fbf3b17729a13739485565f771c06fe6
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-17 08:36:25 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function sync(){
     let osmid = $("#osmid").val();
     fetch('https://osm.hisgis.nl/api/0.6/way/' + osmid + '/')
-  .then((response) => response.xml())
-  .then((data) => console.log(data));
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => verwerkPerceel(data.getElementById(osmid).getElementsByName("tag")));
+}
+
+function verwerkPerceel(tags){
+    for(let t of tags){
+        $("#uitvoer").text += t.getAttribute("k") + '=' + t.getAttribute("v");
+    }
 }
