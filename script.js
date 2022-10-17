446a1a92cd06b17f8a8bc20ad033ca717a7988f8
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-17 16:56:41 +0200

    wb

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function sync(){
         .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
         .then(data => verwerkPerceel(data.getElementById(osmid).getElementsByTagName("tag")));
      fetch('https://data.hisgis.nl/w/api.php?action=wbgetentities&ids=Q101&format=json')
-         .then(response => verwerkWB(response.json().entities.Q101));
+        .then(response => console.log(response.json()))
+        .then(response => verwerkWB(response.json().entities.Q101));
 }
 
 function verwerkPerceel(tags){
